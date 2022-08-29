34bad2a7d8b4a7b9f95b8bc02a9795425500ea7d
sagar kumar <sagarswaroop00@gmail.com>
2022-08-29 17:12:31 +0530

    show reversal button with cash sale create update done

--- src/FileCabinet/SuiteScripts/SEIU_COPE_Transmittal_Form_Reversal_Userevent.js
+++ src/FileCabinet/SuiteScripts/SEIU_COPE_Transmittal_Form_Reversal_Userevent.js
@@ -1,37 +1,58 @@
 /**
  *@NApiVersion 2.1
  *@NScriptType UserEventScript
+ *@author Sagar Kumar
+ *@description Show "Reversla" button when Cope form is approved and cash sales are also created for all Attched cash deposit with current Cope record.
  */
-define(["N/ui/serverWidget", "N/record"], function (serverWidget, record) {
+define(["N/ui/serverWidget", "N/record", "N/search"], function (
+  serverWidget,
+  record,
+  search
+) {
+  function getcustomerId(recordId) {
+    const localList = record.load({
+      type: "customrecord_localized",
+      id: recordId,
+    });
+    const custId = localList.getValue({
+      fieldId: "custrecord_lz_customer",
+    });
+
+    return custId;
+  }
+
+  function checkRecord(cdID, currentRecord) {
+    log.debug(`${cdID} and ${getcustomerId(currentRecord)}`);
+    var cashsaleSearchObj = search.create({
+      type: "cashsale",
+      filters: [
+        ["type", "anyof", "CashSale"],
+        "AND",
+        ["custbody_je_source_record", "anyof", cdID],
+        "AND",
+        ["applyingtransaction.number", "isempty", ""],
+        "AND",
+        ["mainline", "is", "T"],
+        "AND",
+        ["name", "anyof", getcustomerId(currentRecord)],
+      ],
+      columns: [search.createColumn({ name: "amount", label: "Amount" })],
+    });
+    var searchResultCount = cashsaleSearchObj.runPaged().count;
+    log.debug(`searchResultCount is ${searchResultCount}`);
+    if (searchResultCount > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
   function beforeLoad(context) {
     const APPROVE = 4;
     var currentRecord = context.newRecord;
     var form = context.form;
-    var islink = false;
-    var qualifyingCD = currentRecord.getValue({
-      fieldId: "custbody_qualifying_cd",
-    });
-    var nonQualifyingCD = currentRecord.getValue({
-      fieldId: "custbody_cd_non_qualifying",
-    });
-    var holdCD = currentRecord.getValue({
-      fieldId: "custbody_cd_hold_acc",
-    });
-
-    var isStatus = currentRecord.getValue({
-      fieldId: "custbody_status",
-    });
-
-    if (qualifyingCD) {
-      islink = true;
-    } else if (nonQualifyingCD) {
-      islink = true;
-    } else if (holdCD) {
-      islink = true;
-    } else {
-      islink = false;
-    }
-    if (islink == true && isStatus == APPROVE) {
+    let copeCDTotal = 0;
+    let cashSaleTotal = 0;
+    try {
       var CopeTramsmittalFormData = {
         CTFInternalId: currentRecord.id,
         qualifyingRecord: currentRecord.getValue({
@@ -52,29 +73,88 @@ define(["N/ui/serverWidget", "N/record"], function (serverWidget, record) {
         title: "CTF Data Obj",
         details: CopeTramsmittalFormData,
       });
-      form.addButton({
-        id: "custpagecancelbutton",
-        label: "Reversal",
-        functionName:
-          "CopeTransmitttalclientscriptreversal(" +
-          JSON.stringify(CopeTramsmittalFormData) +
-          ")",
-      });
-      form.clientScriptModulePath =
-        "SuiteScripts/SEIU_COPE_tranmittal_form_clientScript_for_reversal.js";
-    }
-  
-    if (context.newRecord.type == "customtransaction108") {
-      let isReversal = false;
-      
-      isReversal = context.currentRecord.getValue({
-        fieldId: "custbody_seiu_ctf_is_reversal_create",
+
+      const localCust = currentRecord.getValue({
+        fieldId: "custbody_local_customer",
       });
 
-      if (isReversal) {
-        window.location.reload;
+      const isStatus = currentRecord.getValue({
+        fieldId: "custbody_status",
+      });
+
+      if (CopeTramsmittalFormData.qualifyingRecord) {
+        copeCDTotal++;
+        cashSaleTotal += checkRecord(
+          CopeTramsmittalFormData.qualifyingRecord,
+          localCust
+        );
       }
+      if (CopeTramsmittalFormData.nonQualifyingRecord) {
+        copeCDTotal++;
+        cashSaleTotal += checkRecord(
+          CopeTramsmittalFormData.nonQualifyingRecord,
+          localCust
+        );
+      }
+      if (CopeTramsmittalFormData.holdCD) {
+        copeCDTotal++;
+        cashSaleTotal += checkRecord(CopeTramsmittalFormData.holdCD, localCust);
+      }
+      if (CopeTramsmittalFormData.adjNonQaulCD) {
+        copeCDTotal++;
+        cashSaleTotal += checkRecord(
+          CopeTramsmittalFormData.adjNonQaulCD,
+          localCust
+        );
+      }
+      if (CopeTramsmittalFormData.adjsutQaulCD) {
+        copeCDTotal++;
+        cashSaleTotal += checkRecord(
+          CopeTramsmittalFormData.adjsutQaulCD,
+          localCust
+        );
+      }
+
+      // if (qualifyingCD) {
+      //   islink = true;
+      // } else if (nonQualifyingCD) {
+      //   islink = true;
+      // } else if (holdCD) {
+      //   islink = true;
+      // } else {
+      //   islink = false;
+      // }
+
+      log.debug(
+        `copeCDTotal is ${copeCDTotal} and cashSaleTotal is ${cashSaleTotal}`
+      );
+
+      if (copeCDTotal == cashSaleTotal && isStatus == APPROVE) {
+        form.addButton({
+          id: "custpagecancelbutton",
+          label: "Reversal",
+          functionName:
+            "CopeTransmitttalclientscriptreversal(" +
+            JSON.stringify(CopeTramsmittalFormData) +
+            ")",
+        });
+        form.clientScriptModulePath =
+          "SuiteScripts/SEIU_COPE_tranmittal_form_clientScript_for_reversal.js";
+      }
+    } catch (error) {
+      log.debug(error);
     }
+    // if (context.newRecord.type == "customtransaction108") {
+    //   let isReversal = false;
+
+    //   isReversal = context.currentRecord.getValue({
+    //     fieldId: "custbody_seiu_ctf_is_reversal_create",
+    //   });
+
+    //   if (isReversal) {
+    //     window.location.reload;
+    //   }
+    // }
   }
   return {
     beforeLoad: beforeLoad,
